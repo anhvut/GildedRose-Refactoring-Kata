54194dd0dd14a5504dd53e3c7e6dab8a45b48b63
Anh-Vu Tran <anhvutran1@gmail.com>
2018-07-14 17:58:23 +0200

    invert if condition: no more test of false expression

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -20,14 +20,7 @@ public class Item {
     }
 
     void updateItem() {
-        if (!name.equals("Aged Brie")
-                && !name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (quality > 0) {
-                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                    quality = quality - 1;
-                }
-            }
-        } else {
+        if (name.equals("Aged Brie") || name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (quality < 50) {
                 quality = quality + 1;
 
@@ -45,26 +38,35 @@ public class Item {
                     }
                 }
             }
+        } else {
+            if (quality > 0) {
+                if (name.equals("Sulfuras, Hand of Ragnaros")) {
+                } else {
+                    quality = quality - 1;
+                }
+            }
         }
 
-        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+        if (name.equals("Sulfuras, Hand of Ragnaros")) {
+        } else {
             sellIn = sellIn - 1;
         }
 
         if (sellIn < 0) {
-            if (!name.equals("Aged Brie")) {
-                if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (quality > 0) {
-                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                            quality = quality - 1;
-                        }
-                    }
-                } else {
-                    quality = quality - quality;
-                }
-            } else {
+            if (name.equals("Aged Brie")) {
                 if (quality < 50) {
                     quality = quality + 1;
+                }
+            } else {
+                if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    quality = quality - quality;
+                } else {
+                    if (quality > 0) {
+                        if (name.equals("Sulfuras, Hand of Ragnaros")) {
+                            return;
+                        }
+                        quality = quality - 1;
+                    }
                 }
             }
         }
